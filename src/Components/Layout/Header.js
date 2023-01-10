631eb8d5ea9d3679bb6110b7c3cb3ac11b2c307a
= <=>
2023-01-10 13:06:32 +0530

    Made the cart button accessible

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -7,7 +7,7 @@ const Header = props => {
     return <Fragment>
         <header className={classes.header}>
             <h1>ReactMeals</h1>
-            <HeaderCartButton>Cart</HeaderCartButton>
+            <HeaderCartButton onAction={props.onCartAccess}>Cart</HeaderCartButton>
         </header>
         <div className={classes['main-image']}>
             <img src={mealsImage} alt="A table full of delicious food!" />          
--- src/Components/Layout/HeaderCartButton.js
+++ src/Components/Layout/HeaderCartButton.js
@@ -1,15 +1,24 @@
 import React from "react";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css"
+import { useContext } from "react";
+import CartContext from "../../store/CartContext";
 
-const HeaderCartButton = props =>{
-    return <button className={classes.button}>
+const HeaderCartButton = (props) =>{
+
+    const cartCTX = useContext(CartContext);
+
+    const numberOfCartItems = cartCTX.items.reduce((currentAmount, item)=>{
+        return currentAmount + item.amount;
+    },0);
+
+    return <button className={classes.button} onClick={props.onAction}>
         <span className={classes.icon}>
             <CartIcon />
         </span>
         <span>Your Cart</span>
         <span className={classes.badge}>
-            3
+            {numberOfCartItems}
         </span>
     </button>
 };
