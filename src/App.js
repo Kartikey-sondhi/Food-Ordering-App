e8f0f40f4b771acda9f1e461270e7b11ac809382
= <=>
2023-01-11 11:47:57 +0530

    App had removed the art conditional content

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import React, { Fragment } from "react";
+import React, {  useState } from "react";
 
 import Header from "./Components/Layout/Header";
 import Meals from "./Components/Meals/Meals";
 import Cart from "./Components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
+  const [showCart,setShowCart] = useState(false);
+
+  const showCartHandler = () => {
+    console.log(showCart);
+    setShowCart(true);
+  }
+  const hideCartHandler = () => {
+    setShowCart(false);
+  }
+
   return (
-    <Fragment>
-      <Cart />
-      <Header />
+    <CartProvider>
+      {showCart && <Cart onClose={hideCartHandler}/>}
+      <Header onCartAccess={showCartHandler}/>
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
